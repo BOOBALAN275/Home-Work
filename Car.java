3b6c19976fca0fc9944fe104471f18dcc3066586
BOOBALAN275 <boobalan272005@gmail.com>
2025-10-08 18:29:22 +0530

    First Day HomeWork DataType & Variable

---
+++ Car.java
@@ -0,0 +1,17 @@
+class Car
+{
+	public static void main(String[] args)
+	{
+		String carName="Tesla Model S";
+		int mileage=396;
+		String fuelType="Electric";
+		float Price=85000.00f;
+		String Brand="Tesla";
+		
+		System.out.println(carName);
+		System.out.println(Brand);
+		System.out.println(fuelType);
+		System.out.println(mileage);
+		System.out.println(Price);
+	}
+}
---
+++ GalaxyS24Ultra.java
@@ -0,0 +1,17 @@
+class GalaxyS24Ultra
+{
+	public static void main(String[] args)
+	{
+		String mobileName="GalaxyS24Ultra";
+		String mobileBrand="Samsung";
+		float screenSize=5.8f;
+		int batteryCapacity=5000;
+		float price=74000.58f;
+		
+		System.out.println(mobileName);
+		System.out.println(mobileBrand);
+		System.out.println(screenSize);
+		System.out.println(batteryCapacity);
+		System.out.println(price);
+	}
+}
