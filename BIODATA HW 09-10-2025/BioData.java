806e5b0282d0dd7cdaef855053d79b5b60da0a01
BOOBALAN275 <145772899+BOOBALAN275@users.noreply.github.com>
2025-10-12 10:44:25 +0530

    Add files via upload
    BioData HomeWork

---
+++ BIODATA HW 09-10-2025/BioData.java
@@ -0,0 +1,29 @@
+class BioData
+{
+	public static void main(String[] args)
+	{
+	String Name="Boobalan";
+	char Inisial='K';
+	String PlaceOfBirth="Madurai";
+	String Education="Bachelor Of Commerce Computer Application";
+	String Occupation="Job Seeker";
+	
+	String FatherName="Karthikeyan";
+	String MotherName="Dhanalakshmi";
+	byte Day=9;
+	byte Month=10;
+	short Year=2025;
+	
+	System.out.println("Personal Details");
+	System.out.println("Name :"+ Name+"."+Inisial);
+    System.out.println("Day Of Brith :"+Day+"."+ Month+"."+ Year);
+	System.out.println("Place Of Birth :"+ PlaceOfBirth);
+	System.out.println("Eduaction :"+ Education);
+	System.out.println("Occupation :"+ Occupation);
+	System.out.println("  ");
+	System.out.println("  ");
+	System.out.println("Family Details");
+	System.out.println("Father :"+ FatherName);
+	System.out.println("Mother :"+ MotherName);
+	}
+}
