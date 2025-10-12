c2b7195a6b928f08e947e5ed8a0066be4808e144
BOOBALAN275 <145772899+BOOBALAN275@users.noreply.github.com>
2025-10-12 10:38:47 +0530

    Add files via upload
    Operators Class

---
+++ HW 10-10-2025/DivideOperators.java
@@ -0,0 +1,11 @@
+class DivideOperators
+{
+	public static void main(String[] args)
+		{
+			int Raj=125;
+			int Ravi=5;
+			int Kumar=Raj/Ravi;
+			
+			System.out.println("THE DIVISION OF TWO VALUES IS:"+Kumar);
+		}
+}
---
+++ HW 10-10-2025/ProductOperators.java
@@ -0,0 +1,11 @@
+class ProductOperators
+{
+	public static void main(String[] args)
+		{
+			int Raj=25;
+			int Ravi=4;
+			int Kumar=Raj*Ravi;
+			
+			System.out.println("THE PRODUCT OF TWO VALUES IS:"+Kumar);
+		}
+}
