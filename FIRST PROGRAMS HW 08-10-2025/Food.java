96bc7e9809aa9586c51e6fa14468f5bba659079e
BOOBALAN275 <145772899+BOOBALAN275@users.noreply.github.com>
2025-10-30 16:42:55 +0530

    Add files via upload
    FIRST PROGRAM

---
+++ FIRST PROGRAMS HW 08-10-2025/Food.java
@@ -0,0 +1,15 @@
+class Food
+{
+	public static void main(String[] args)
+	{
+		String foodName="Paneer Butter Masala";
+		int calories=400;
+		boolean Vegetarian=true;
+		float price=250.50f;
+		
+		System.out.println(foodName);
+		System.out.println(calories);
+		System.out.println(Vegetarian);
+		System.out.println(price);
+	}
+}
---
+++ FIRST PROGRAMS HW 08-10-2025/Mobile.java
@@ -0,0 +1,17 @@
+class Mobile
+{
+	public static void main(String[] args)
+	{
+		String mobileName="iphone 15 Pro";
+		String mobileBrand="Apple";
+		float screenSize=6.1f;
+		int batteryCapacity=3274;
+		float price=134999.99f;
+		
+		System.out.println(mobileName);
+		System.out.println(mobileBrand);
+		System.out.println(screenSize);
+		System.out.println(batteryCapacity);
+		System.out.println(price);
+	}
+}
